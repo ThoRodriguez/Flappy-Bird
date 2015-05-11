a1b7366f26536667a16aced0d6d6f500b57189d2
ThoRodriguez <rdgz.thomas@gmail.com>
2015-05-11 12:05:09 +0200

    Ajouté lundi depuis le lycée à 12h00. Ajout d'une police. Correction de BUGs

--- flappybird.py
+++ flappybird.py
@@ -17,21 +17,60 @@ pygame.display.set_caption("Flappy Bird")
 fond = pygame.image.load("fondflappy.png")
 oiseau = pygame.image.load("flappy2.png")
 
-def flappy(oiseau):
-    surface.blit(oiseau,(150,200))
 
+
+def flappy(x,y,oiseau):
     
-def main():    
+    surface.blit(oiseau,(x,y))
+    
+
+def GO():
+    message("Game Over")
+
+def message(texte):
+    gameover = pygame.font.Font("04b.ttf",140)
+    rejouer = pygame.font.Font("04b.ttf",30)
+    gameoversurface, gameoverRect = creaTexte(texte, gameover) #contient 1 texte et 1 police
+    gameoverRect.center = surfaceL/2, (surfaceH/2)-50
+    surface.blit(gameoversurface, gameoverRect)
+    
+    rejouersurface, rejouerRect = creaTexte("Rejouer ?", rejouer)
+    rejouerRect.center = surfaceL/2, (surfaceH/2)+50
+    surface.blit(rejouersurface, rejouerRect)
+    
+    pygame.display.update()
+    
+    
+def creaTexte(texte, police):
+    white = (255,255,255)
+    textesurface = police.render(texte, True, white)
+    return textesurface, textesurface.get_rect()
+
+
+def main(): 
+    x = 150
+    y = 200
+    y_mouv = 0
     game_over = False
     while (game_over == False) :  #quitter boucle inf
         for event in pygame.event.get(): #recherche parmi evenements
             if event.type == pygame.QUIT:
                 game_over = True 
-           
-    surface.blit(fond,(0,0))         
-    flappy(oiseau)
-    pygame.display.update()
-
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    y_mouv = -10
+            if event.type == pygame.KEYUP:
+                y_mouv = 3
+        y += y_mouv
+        if y>surfaceH-5 or y <-5: #2 bords en hauteur
+            GO()
+            pygame.quit()
+        surface.blit(fond,(0,0))         
+        flappy(x,y, oiseau)
+        pygame.display.update() #rafraichissement
+            
+    
+        
 main()
 pygame.quit()
 quit()
