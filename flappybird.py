47f4e730a7a5953e608f25eaf6f058b22deb702f
MaxThibert <max.cdg@hotmail.fr>
2015-05-18 19:27:56 +0200

    correction bug + début collision

--- flappybird.py
+++ flappybird.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat May 16 01:57:21 2015
+Created on Thu May 14 21:31:35 2015
 
 @author: Thomas Rodriguez et Maxime Thibert
-"""
 
+Demander pour les nombreux if (elif ?) et nombreuses variables ds fonction main
+"""
 import pygame
 import time
 import random
@@ -22,9 +23,14 @@ def flappy(x,y,oiseau):
     
     surface.blit(oiseau,(x,y))
     
-def tuyaux(tuyau,x_tuyau,y_tuyau):
+def tuyaux(tuyau,tuyau_bas,x_tuyau,y_tuyau,ecart): 
     surface.blit(tuyau,(x_tuyau,y_tuyau))
-   
+    surface.blit (tuyau_bas,(x_tuyau,y_tuyau+509+ecart)) #509 = taille tuyau
+
+def bord_defile(bord,x_bord,y_bord):
+    surface.blit(bord,(x_bord,y_bord))
+
+
 
 def message(texte,surfaceL,surfaceH):
     gameover = pygame.font.Font("04b.ttf",140)
@@ -41,16 +47,9 @@ def message(texte,surfaceL,surfaceH):
     
     
 def creaTexte(texte, police):
-    white = (255, 255, 255)
+    white = (255,255,255)
     textesurface = police.render(texte, True, white)
     return textesurface, textesurface.get_rect()
-
-
-def Score(score):
-    black = (0, 0, 0)    
-    font = pygame.font.Font(None ,50)
-    textesurface = font.render(("Score: "+str(score)), True, black)
-    surface.blit(textesurface, [0, 0])
 
 
 def rejoueOUquitte():
@@ -62,52 +61,41 @@ def rejoueOUquitte():
             continue
         return event.key
     return None
-
 import pygame, sys
 import pygame.locals
 
 
-def defilement():
-    pygame.init()
- 
-    ecran = pygame.display.set_mode((1280, 720))
- 
-    clock = pygame.time.Clock()
- 
-    background1 = pygame.image.load('fondflappy.png')
-    background2 = pygame.image.load('fondflappy.png')
-     
-    abscisse_background1 = 0
-    abscisse_background2 = background1.get_left()
- 
-    while True:
- 
-        ecran.blit(background1, (0, abscisse_background1))
-        ecran.blit(background2, (0,abscisse_background2))
- 
-        pygame.display.update()
- 
-        abscisse_background1 -= 1
-        abscisse_background2 -= 1
-    
-        if abscisse_background1 == -1 * background1.get_left():
-            abscisse_background1 =abscisse_background2 + background2.get_left()
-        if background2 == -1 * background2.get_left():
-            abscisse_background2 = abscisse_background1 + background1.get_left()
- 
-    clock.tick(60)
+def Score(score,x_tuyau):
+    black = (0, 0, 0)    
+    font = pygame.font.Font(None ,50)
+    textesurface = font.render(("Score: "+str(score)), True, black)
+    surface.blit(textesurface, [0, 0])
 
+    
+def collision(xf,yf,xt,yt,ecart):
+    if xf > xt-135 and xf < xt+75 and (yf >= yt+509 or yf <= yt+509+ecart):
+        message("Game over",surfaceL,surfaceH)
+    
+    
 
 def main(): 
     fond = pygame.image.load("fondflappy.png")
     oiseau = pygame.image.load("flappy2.png")
     tuyau = pygame.image.load("tuyau.png")
+    bord = pygame.image.load("bord.png")
+    tuyau_bas =  pygame.transform.rotate(tuyau, 180)
+    x_flappy = 68
+    y_flappy = 50
     x = 150
     y = 200
     y_mouv = 0
     x_tuyau = surfaceL
-    y_tuyau = random.randint(-380,-200)
+    y_tuyau = random.randint(-400,-280)
+    ecart = 3*y_flappy
     tuyau_vitesse = 2
+    score = 0
+    x_bord = 16
+    y_bord = 470
     game_over = False
     while (game_over == False) :  #quitter boucle inf
         for event in pygame.event.get(): #recherche parmi evenements
@@ -121,14 +109,27 @@ def main():
         y += y_mouv
         if y>surfaceH-5 or y <-5: #2 bords en hauteur
             message("Game over",surfaceL,surfaceH)
+        
         if x_tuyau < (0):
             x_tuyau =  surfaceL
-            y_tuyau = random.randint(-380,-200)
+            y_tuyau = random.randint(-400,-280)
+            score +=1
+            if score >= 2:
+                ecart = 2*y_flappy
+                tuyau_vitesse = score
+                if score >= 10:
+                    tuyau_vitesse = 10
+                
         surface.blit(fond,(0,0))         
         flappy(x,y, oiseau)
-        tuyaux(tuyau,x_tuyau,y_tuyau)
+        tuyaux(tuyau,tuyau_bas,x_tuyau,y_tuyau, ecart)
         x_tuyau -= tuyau_vitesse # bouge de 6 pixels
-        #defilement()
+        Score(score,x_tuyau)
+        #collision(x_flappy,y_flappy,x_tuyau,y_tuyau,ecart)
+        if x_bord < -339 :
+            x_bord = 16
+        bord_defile(bord,x_bord,y_bord)
+        x_bord -= tuyau_vitesse
         pygame.display.update() #rafraichissement
         
         
